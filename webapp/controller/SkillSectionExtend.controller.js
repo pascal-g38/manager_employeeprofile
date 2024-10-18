c2390c2eb77a2a1853265137a40df0f4eec465b5
pascal-g38 <pascalgolomb@outlook.de>
2024-10-18 14:33:00 +0200

    Changed EmployeeSkills to be loaded from the Entity _EmployeeSkills, and not through an Filter

--- webapp/controller/SkillSectionExtend.controller.js
+++ webapp/controller/SkillSectionExtend.controller.js
@@ -11,21 +11,6 @@ sap.ui.define([
         override: {   
             onInit() { 
                 this.getView().byId(this._sectionPrefix + "gridListId");
-                this.getView().setModel(
-                    new JSONModel({
-                        id: null,
-                    }),
-                    "customView"
-                )
-            },
-            routing: {
-                async onAfterBinding() {
-                    this.getView().getModel("customView").setProperty("/id", (await this.getView().getBindingContext().requestProperty("EmployeeId")) )
-                    const SkillsView = this.getView().byId(this._sectionPrefix + "gridListId");
-                    const ItemsBinding = SkillsView.getBinding("items");
-                    const id = this.getView().getModel("customView").getProperty("/id")
-//                    ItemsBinding.filter(new Filter({ path: 'EmployeeId', operator: FilterOperator.EQ , value1: id }));
-                }
             }
            }      
           });
